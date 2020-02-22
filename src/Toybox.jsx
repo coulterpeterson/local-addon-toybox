c42869ff6e97fd8228d63b6d14de839bc992d178
Coulter Peterson <coulter.peterson@p2c.com>
2020-02-21 16:35:48 -0800

    Updated boilerplate to read as Toybox in UI

---
+++ src/Toybox.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Toybox extends Component {
+	render() {
+		return (
+			<div style={{ flex: "1", overflowY: "auto" }}>
+				<h2>Hello World!</h2>
+			</div>
+		);
+	}
+}
